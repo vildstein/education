14990257de5f2db33c531e24e86478d804ae7aec
vildstein <ng75@mail.ru>
2023-02-09 21:46:09 +0600

    task No 10 about serial number of Fibonacci was added

--- main.cpp
+++ main.cpp
@@ -5,27 +5,30 @@ using namespace std;
 
 int main()
 {
-    int null_number = 0;
-    int first_number = 1;
+    int null_number   = 0;
+    int first_number  = 1;
     int second_number = 1;
-    int third_number = 2;
+    int wrong_number = -1;
 
-    int i = 4;
+    int i = 3;
+
+    int number;
 
     int fib = 0;
 
 
-    int number;
 
     cin >> number;
 
- //   0 1 1 2 3 5 8 13 21 34
-    if (number == 0) {cout << null_number << endl;}
-    else if (number == 1) {cout << first_number << endl;}
-    else if (number == 2) {cout << first_number << endl;}
-    else if (number == 3) {cout << third_number << endl;}
-    else if (number > 2) {
-        while ( i <= number )
+ //   0 1 1 2 3 5 8 13 21 34 55     Fibonacci number
+ //   0 1 2 3 4 5 6  7  8  9 10     serial number
+
+    if      (number == 0) {i = number; cout << i << endl;}
+    else if (number == 1) {i = number; cout << i << endl;}
+    else if (number == 2) {i = 3;      cout << i << endl;}
+    else if (number == 3) {i = 4;      cout << i << endl;}
+    else if (number > 3) {
+        while ( fib < number )
         {
             i = i + 1;
 
@@ -34,8 +37,15 @@ int main()
             first_number = second_number - first_number;
 
             fib = second_number + first_number;
+
         }
-    cout << fib << "\n";
+
+//        cout << fib << endl;
+//        cout << i << endl;
+
+        if (number == fib) {cout << i << endl;}
+        else {cout << wrong_number << endl;}
+
     }
 
 
