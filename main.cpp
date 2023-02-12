329967051dd8674d0bc7ad895b9ccd5d91998243
vildstein <ng75@mail.ru>
2023-02-12 18:42:32 +0600

    chapter about double numbers started. Task 2 added. Real number of course 1.6 -2

--- main.cpp
+++ main.cpp
@@ -1,53 +1,21 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 
 int main()
-{
-    int null_number   = 0;
-    int first_number  = 1;
-    int second_number = 1;
-    int wrong_number = -1;
+ {
+    double a;
 
-    int i = 3;
+    cin >> a;
 
-    int number;
+    int result_intermediate;
+    int result;
 
-    int fib = 0;
+    result_intermediate = trunc(a * 10);
 
+    result = result_intermediate % 10;
 
+    cout << result << endl;
 
-    cin >> number;
-
- //   0 1 1 2 3 5 8 13 21 34 55     Fibonacci number
- //   0 1 2 3 4 5 6  7  8  9 10     serial number
-
-    if      (number == 0) {i = number; cout << i << endl;}
-    else if (number == 1) {i = number; cout << i << endl;}
-    else if (number == 2) {i = 3;      cout << i << endl;}
-    else if (number == 3) {i = 4;      cout << i << endl;}
-    else if (number > 3) {
-        while ( fib < number )
-        {
-            i = i + 1;
-
-            second_number = first_number + second_number;
-
-            first_number = second_number - first_number;
-
-            fib = second_number + first_number;
-
-        }
-
-//        cout << fib << endl;
-//        cout << i << endl;
-
-        if (number == fib) {cout << i << endl;}
-        else {cout << wrong_number << endl;}
-
-    }
-
-
-return 0;
-}
+ }
